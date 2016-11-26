722fa38af51d26b9262989f27ee49b73decd0f27
dragon <rongjihuang@gmail.com>
2016-11-26 16:16:03 +0800

    add http server

--- scripts/http.server.js
+++ scripts/http.server.js
@@ -1,5 +1,6 @@
 // https://github.com/koajs/static
 var serve = require('koa-static');
+var path = require('path');
 // https://github.com/alexmingoia/koa-router
 var Router = require('koa-router');
 var koa = require('koa');
@@ -8,26 +9,27 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 // static files
-app.use(serve(__dirname));
+const rooDir = path.resolve(__dirname, "../");
+app.use(serve(rooDir));
 
 // dynamic data
 var dataRouter = new Router({
-	prefix: '/data'
+  prefix: '/data'
 });
-dataRouter.get('/grid.json', function* (next) {
-	var date = [1,2,3,4,5];
+dataRouter.get('/grid.json', function*(next) {
+  var date = [1, 2, 3, 4, 5];
 
-	// respone
-	this.type = "json";
-	this.body = JSON.stringify(date);
+  // respone
+  this.type = "json";
+  this.body = JSON.stringify(date);
 });
 app.use(dataRouter.routes()).use(dataRouter.allowedMethods());
 
 // start http server
 app.listen(port, hostname, () => {
-	console.log(`view demo at http://${hostname}:${port}/demo/index.html`);
+  console.log(`view demo at http://${hostname}:${port}/demo/index.html`);
 });
 
-app.on('error', function (err) {
-	log.error('server error', err);
+app.on('error', function(err) {
+  log.error('server error', err);
 });
